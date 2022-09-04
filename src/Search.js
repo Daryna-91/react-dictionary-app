8a1a348ef52d58f84026c2b593fed1a2b0be449d
Daryna-91 <darynalytvynenko@gmail.com>
2022-09-04 15:11:59 +0300

    added meaning section

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
-
+import Results from "./Results";
 export default function Search() {
   let [word, setWord] = useState("");
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
   function handleSearch(event) {
     event.preventDefault();
@@ -17,8 +19,11 @@ export default function Search() {
     setWord(event.target.value);
   }
   return (
-    <form onSubmit={handleSearch}>
-      <input type="search" onChange={handleChange} />
-    </form>
+    <div>
+      <form onSubmit={handleSearch}>
+        <input type="search" onChange={handleChange} />
+      </form>
+      <Results results={results} />
+    </div>
   );
 }
